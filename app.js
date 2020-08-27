b2b836995a3b0e1b6193b90cf72e7e1dd9b558a3
allgoodmans571 <allgoodmans571@gmail.com>
2020-08-27 14:14:00 +0300

    no message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use('/public', express.static(__dirname + '.public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
 
